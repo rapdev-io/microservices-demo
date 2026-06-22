d8afe2402a23538749c72b3a01027385e55e9076
Matt Morris <Matthew.w.morris@hotmail.com>
2026-06-22 14:46:52 +0000

    SDE-28: update recommendation_server.py

--- src/recommendationservice/recommendation_server.py
+++ src/recommendationservice/recommendation_server.py
@@ -159,7 +159,6 @@ if __name__ == "__main__":
         
     # while True: # Add infinite loop so product catalogs are never retrieved
     #    logger.error("Stuck in loop") #log a quick message for troubleshooting
-     #    
     
     # Create gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
